367340f344b98401d9b36717d59512256fcc4e5c
Christoph Könekamp <Christoph.koenekamp@icloud.com>
2022-07-13 15:07:29 +0100

    update

--- commands/nerve-links.js
+++ commands/nerve-links.js
@@ -1,21 +1,18 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed, MessageActionRow, MessageSelectMenu, CommandInteraction } = require(`discord.js`)
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('embed')
 		.setDescription('Replies with Embed!'),
-	async execute(message, args, Discord) {
-            const newEmbed = new Discord.MessageEmbed()
+
+	async execute(interaction) {
+            const newEmbed = new MessageEmbed()
             .setColor('#0x00f2fc')
             .setTitle('Nerve Global Essential Links')
-            .addFields(
-                {name: 'Websites', vaue: 'Homepage: https://www.nerveglobal.com/\nDocumentation: https://docs.nerveglobal.com/\nGitHub: https://github.com/nerveglobal'},
-                {name: 'Stores', vaue: 'Apple App Store: https://apps.apple.com/de/app/nerve-global/id1500517863\nGoogle Play Store: https://play.google.com/store/apps/details?id=com.academy.nerve&hl=en&gl=US'},
-                {name: 'Social Links', vaue: 'Twitter: https://twitter.com/nerveglobal_\nTelegram: https://t.me/nerveglobal\nFacebook: https://www.facebook.com/nerveglobal\nInstagram: https://www.instagram.com/nerveglobal\nLinkedIn: https://www.linkedin.com/company/nerveglobal'}
-            )
-            .setFooter('Last Updated: July 12th, 2022');
+            .setFooter({text: 'Last Updated: July 12th, 2022'});
     
-            message.channel.send(newEmbed);
-		return message, args, Discord.reply()
+            interaction.channel.send({embeds: [newEmbed]});
+		return interaction.reply()
 	},
 };
